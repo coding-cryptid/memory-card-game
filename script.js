e6a91321fa38601238e5832d4649c6b90e3c0664
Gianna Cosenza <giacosey28@gmail.com>
2026-03-17 13:15:12 -0400

    created shuffle function to randomize the cards

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const restartBtn = document.getElementById("restart-btn");
 
 const icons = ["🧠", "☕️", "💻", "💡", "🩷", "🥡", "🍔", "🍟"];
 let cards = [...icons, ...icons];
+
+function suffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
