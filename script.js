4c29d4ae89e0ab4be3e4b58f6ac5ecb236de2dee
Gianna Cosenza <giacosey28@gmail.com>
2026-03-17 13:13:06 -0400

    defined gameboard and icons, cards vaiables

---
+++ script.js
@@ -0,0 +1,5 @@
+const gameBoard = document.getElementById("game-board");
+const restartBtn = document.getElementById("restart-btn");
+
+const icons = ["🧠", "☕️", "💻", "💡", "🩷", "🥡", "🍔", "🍟"];
+let cards = [...icons, ...icons];
